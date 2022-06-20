42b215eb640ff882fadb7e4040b1750f36732f6a
Seeun Park <worldag17@gmail.com>
2022-06-21 01:22:25 +0900

    '게시글 작성 페이지' 수정

--- Connecting/WebContent/board/board_write.jsp
+++ Connecting/WebContent/board/board_write.jsp
@@ -5,135 +5,81 @@
 	<meta charset="utf-8">
 	<title>Board Write</title>
 	<link rel="icon" href="<%=request.getContextPath()%>/resources/img/connecting/favicon.ico" />
-	<style>
-		
-		.a {
-			border:none;
-		}
-		
-		.a:focus{
-			outline:none;
-		}
-	
-		.imgupload{
-			display:inline-block;
-		}
-		
-		.content{
-			display:inline-block;
-		}
-		
-		td{
-			border-bottom:1px solid black;
-		}
-		
-		tr{
-			height:50px;
-		}
-		
-		table{
-			border-bottom:1px solid black;
-		}
-
-		.form_header{
-			display:flex;
-			justify-content:center;
-		}
-		
-		.sel{
-			margin:30px 30px;
-			display:flex;
-			justify-content:center;
-		}
-
-
-	</style>
+	<link rel="stylesheet" href="<%=request.getContextPath()%>/resources/css/boardForm.css" />
 </head>
 <body>
     <jsp:include page="../main/header.jsp" />
     
-    <div class="container cont_form">
-    	<form class="board_form" name="boardWrite" action="boardAddAction.bo" method="post" enctype="Multipart/form-data">
-
-    	<div class="sel">
-	   		<select name="category">
-	   			<option value="">카테고리</option>
-	   			<option value="0">전시회</option>
-	   			<option value="1">박람회</option>
-	   			<option value="2">버스킹</option>
-	   			<option value="3">연극&#47;공연</option>
-	   		</select>
-	  		<select name="loc">
-	      		<option value="">지역</option>
-	   			<option value="0">서울</option>
-	   			<option value="1">경기&#47;인천</option>
-	   			<option value="2">대전&#47;충청&#47;강원</option>
-	   			<option value="3">부산&#47;대구&#47;경상</option>
-	   			<option value="4">광주&#47;전라&#47;제주</option>
-	   		</select>
-		</div>
-
-		<div class="form_header">
-		<div class="imgupload">
-			<label>
-				<span id="showImage">
-					<img style="width:300px" src="images/nonimg.png" width="140px" alt="대표이미지">
-				</span>
-				<input type="file" style="display:none"
-	                   id="board_img" name="board_img"
-	                   accept="image/*" />
-			</label>
-		</div>
-		
-       &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-       <div class="content">
-       	<table>
-	      <thead>
-	         <tr>
-	            <td colspan="2">
-	               <input class="a" type="text" maxlength="50" style="width:300px" placeholder="제목을 입력하세요..." name="title">
-	            </td>
-	         </tr>
-	      </thead>
-	      <tbody>
-	         <tr>
-	            <th>주최자명</th>
-	            <td><input class="a" type="text" name="host"></td>
-	         </tr>
-	         <tr>
-	            <th>장소</th>
-	            <td><input class="a" type="text" name="address"></td>
-	         </tr>
-	         <tr>
-	            <th>날짜</th>
-	            <th><input type="date" name="start_date">
-	               ~
-	               <input type="date" name="end_date">
-	            </th>
-	         </tr>
-	         <tr>
-	            <th>시간</th>
-	            <th><input type="time" name="start_time">
-	               ~
-	               <input type="time" name="end_time">
-	            </th>
-	         </tr>
-	      </tbody>
-        </table>
-       </div> 
-    	</div>
-		       
-                 
-                   
-                     
-		<div class="form-group">
-			<label for="content" class="form-label mt-4">본문</label>
-      		<textarea class="form-control" name="content" id="content" rows="15" cols="600" style="font-size:23px" placeholder="내용을 입력하세요."></textarea>
-    	</div>
-  			<button type="submit" class="btn btn-primary btn-lg">제출</button>
-		  
+    <div class="container board-form">
+    	<form name="boardWrite" action="boardAddAction.bo" method="post" enctype="Multipart/form-data">
+			<div class="form-group row">
+				<!-- 이미지 업로드 -->
+				<div class="col-md-6" id="showImage">
+					<label>
+						<span id="imgUpdate">
+							<img id="boardImg" src="./resources/img/default-img.png" alt="이미지 업로드">
+						</span>
+						<input type="file" id="editImg" name="board_img" accept="image/*" />
+					</label>
+				</div>
+				<div class="col-md-6 board-info">
+					<!-- 카테고리 및 지역 select -->
+					<div class="sel">
+						<select name="category">
+							<option value="">카테고리</option>
+							<option value="0">전시회</option>
+							<option value="1">박람회</option>
+							<option value="2">버스킹</option>
+							<option value="3">연극&#47;공연</option>
+						</select>
+						<select name="loc">
+							<option value="">지역</option>
+							<option value="0">서울</option>
+							<option value="1">경기&#47;인천</option>
+							<option value="2">대전&#47;충청&#47;강원</option>
+							<option value="3">부산&#47;대구&#47;경상</option>
+							<option value="4">광주&#47;전라&#47;제주</option>
+						</select>
+					</div>
+					<!--  게시글 정보 입력  -->
+					<table>
+						<thead>
+							<tr>
+								<td colspan="2">
+									<input class="input-text" id="title" type="text" maxlength="30" placeholder="제목을 입력하세요..." name="title">
+								</td>
+							</tr>
+						</thead>
+						<tbody>
+							<tr>
+								<th>주최자명</th>
+								<td><input class="input-text" type="text" name="host"></td>
+							</tr>
+							<tr>
+								<th>장소</th>
+								<td><input class="input-text" type="text" name="address"></td>
+							</tr>
+							<tr>
+								<th>날짜</th>
+								<th><input type="date" name="start_date"> &#126; <input type="date" name="end_date"></th>
+							</tr>
+							<tr>
+								<th>시간</th>
+								<th><input type="time" name="start_time"> &#126; <input type="time" name="end_time"></th>
+							</tr>
+						</tbody>
+					</table>
+				</div>
+			</div>
+			<div class="form-group">
+				<label for="content" class="form-label mt-4">본문</label>
+				<textarea class="form-control" id="content" rows="15" placeholder="내용을 입력하세요..." name="content"></textarea>
+			</div>
+			<div class="mt-4 text-center">
+				<button type="submit" class="btn btn-success btn-lg">작성 완료</button>			
+			</div>
 		</form>
-    </div>
+	</div>
  	
 	<script src="<%=request.getContextPath()%>/resources/js/board_write.js"></script>
 </body>
--- Connecting/WebContent/resources/js/board_write.js
+++ Connecting/WebContent/resources/js/board_write.js
@@ -1,170 +1,134 @@
 $(document).ready(function() {	
+	
+	// 게시글 이미지 추가
+	$('input[type=file]').change(function(event) {
+		const inputfile = $(this).val().split('\\');
+		const filename = inputfile[inputfile.length - 1];
+		const pattern = /(gif|jpg|jpeg|png)$/i;
+		if(pattern.test(filename)) {
+			const reader = new FileReader(); // 파일을 읽기 위한 객체 생성
+			// DataURL 형식으로 파일 읽어오기
+			reader.readAsDataURL(event.target.files[0]);
+			reader.onload = function(event) {// 읽기에 성공했을 때 실행되는 이벤트 핸들러
+				$('#imgUpdate').html('<img id="boardImg" src="' + event.target.result + '">');	
+			};
+		} else {
+			alert('확장자는 gif, jpg, jpeg, png 사용 가능합니다.')
+			$(this).val("");
+		}
+	});
+	
+	$('form').submit(function() {
+		if ($('select[name = category]').val() == "") { //카테고리
+			alert("카테고리를 선택하세요.");
+			return false;
+		}
 
-	$('input[type=file]').change(function(event){
-			var inputfile = $(this).val().split('\\');
-			var filename = inputfile[inputfile.length - 1];
-			var pattern = /(gif|jpg|jpeg|png)$/i;
-			if(pattern.test(filename)){
-				var reader = new FileReader(); // 파일을 읽기 위한 객체 생성
-				// DataURL 형식으로 파일 읽어오기
-				reader.readAsDataURL(event.target.files[0]);
-				reader.onload = function(event) {// 읽기에 성공했을 때 실행되는 이벤트 핸들러
-					$('#showImage').html('<img width="300px" src="' + event.target.result + '">');	
-				};
-			} else {
-				alert('확장자는 gif, jpg, jpeg, png 사용 가능합니다.')
-				$(this).val("");
-			}
-		console.log($('#change_img').val());
-	})
-	
-	
-	
-	
-	$('.board_form').submit(function(){
-		
-		//카테고리
-		if($('select[name = category]').val() == ""){
-			alert("카테고리를 선택하세요.")
+		if ($('select[name = loc]').val() == "") { //지역
+			alert("지역을 선택하세요.");
 			return false;
 		}
-		
-		//지역
-		if($('select[name = loc]').val() == ""){
-			alert("지역을 선택하세요.")
+
+		if ($('#board_img').val() == "") { // 게시글 이미지
+			alert("게시글 사진은 필수입니다.");
 			return false;
 		}
-		
-		//이미지
-		if($('#board_img').val()==""){
-			alert("사진을 반드시 첨부해 주세요.")
-			return false;
-		}
-		
-		//제목
-		if($('input[type = text]').eq(1).val()==""){
-			alert("제목을 입력해 주세요.")
+
+		if ($('input[type = text]').eq(1).val() == "") { // 제목
+			alert("제목은 필수입니다.");
 			$('input[type = text]').eq(1).focus();
 			return false;
 		}
-		
-		//주최자명
-		if($('input[type = text]').eq(2).val()==""){
-			alert("주최자명을 입력해 주세요.")
+
+		if ($('input[type = text]').eq(2).val() == "") { // 주최명
+			alert("주최명을 입력해 주세요.");
 			$('input[type = text]').eq(2).focus();
 			return false;
 		}
-		
-		//장소
-		if($('input[type = text]').eq(3).val()==""){
-			alert("장소를 입력해 주세요.")
+
+		if ($('input[type = text]').eq(3).val() == "") { //장소
+			alert("장소를 입력해 주세요.");
 			$('input[type = text]').eq(3).focus();
 			return false;
 		}
-		
-		//날짜
-		if($('input[name = start_date]').val()==""){
-			
-			alert("시작날짜를 입력해 주세요.")
-			return false;
-		}
-		
-		if($('input[name = end_date]').val()==""){
-			alert("종료날짜를 입력해 주세요.")
-			return false;
-		}
-		
-		//시간
-		if($('input[name = start_time]').val()==""){
-			
-			alert("시작시간을 입력해 주세요.")
-			return false;
-		}
-		
-		if($('input[name = end_time]').val()==""){
-			alert("종료시간을 입력해 주세요.")
-			return false;
-		}
-		
-		if($('#content').val()==""){
-			alert("내용을 입력해 주세요.")
-			return false;
-		}
-		
-		//사진 변경 안했을시 값설정
-		if(!$('#change_img').val()){
-			var img_val = $('#filevalue').text();
-			console.log($('#change_img').val());
-			console.log(img_val);
-			console.log($('#change_img').val());
-			html = "<input type='hidden' value='" + img_val + "' name='check'>";
-			$(this).append(html);
-		}
-		
-	})
-	
-	
-	//현재 날짜와 종료날짜 비교
-	
-	
-	
-	//시작날짜와 종료날짜 비교
-	$('input[name = end_date]').change(function(){
-		console.log("end_date")
-		
-		//yyyy-mm-dd
-		var sysdate = new Date(+new Date() + 3240 * 10000).toISOString().split("T")[0];
 
-		var end_date = $('input[name = end_date]').val();
-		var start_date = $('input[name = start_date]').val();
-		
-		if(start_date == ""){
-			alert("시작날짜를 먼저 설정해 주세요.")
-			$('input[name = end_date]').val("");
-		}else if(end_date < sysdate){
-			alert("지난 날짜 입니다.")
-			$('input[name = end_date]').val("");
-		}else if(start_date > end_date){
-			alert("종료날짜를 시작날짜와 같거나 이후로 설정해 주세요.")
-			$('input[name = end_date]').val("");
+		// 날짜
+		if ($('input[name = start_date]').val() == "") {
+			alert("시작날짜를 입력해 주세요.");
+			return false;
 		}
-		
+
+		if ($('input[name = end_date]').val() == "") {
+			alert("종료날짜를 입력해 주세요.");
+			return false;
+		}
+
+		//시간
+		if ($('input[name = start_time]').val() == "") {
+			alert("시작시간을 입력해 주세요.");
+			return false;
+		}
+
+		if ($('input[name = end_time]').val() == "") {
+			alert("종료시간을 입력해 주세요.");
+			return false;
+		}
+
+		if ($('#content').val() == "") {
+			alert("내용을 입력해 주세요.");
+			return false;
+		}
 	})
 	
-	//시작시간과 종료시간 비교
-	$('input[name = end_time]').change(function(){
-		var end_time = $('input[name = end_time]').val();
-		var start_time = $('input[name = start_time]').val();
-		var end_date = $('input[name = end_date]').val();
-		var start_date = $('input[name = start_date]').val();
-		
-		
-		if(start_time == ""){
-			alert("시작시간을 먼저 설정해 주세요.")
+	// 시작날짜와 종료날짜 비교
+	$('input[name = end_date]').change(function() {
+		const sysdate = new Date(+new Date() + 3240 * 10000).toISOString().split("T")[0]; // yyyy-mm-dd
+		const end_date = $('input[name = end_date]').val();
+		const start_date = $('input[name = start_date]').val();
+
+		if (start_date == "") {
+			alert("시작일를 먼저 설정해 주세요.");
+			$('input[name = end_date]').val("");
+		} else if (end_date < sysdate) {
+			alert("지난 날짜입니다.");
+			$('input[name = end_date]').val("");
+		} else if (start_date > end_date) {
+			alert("종료일를 시작일과 같거나 이후로 설정해 주세요.");
+			$('input[name = end_date]').val("");
+		}
+
+	})
+	
+	// 종료날짜 설정 후 시작날짜 변경 시
+	$('input[name = start_date]').change(function() {
+		const sysdate = new Date(+new Date() + 3240 * 10000).toISOString().split("T")[0];
+		const end_date = $('input[name = end_date]').val();
+		const start_date = $('input[name = start_date]').val();
+
+		if (start_date != "" && end_date != "") {
+			if (start_date > end_date) {
+				alert("시작일을 종료일과 같거나 이전로 설정해 주세요.");
+				$('input[name = start_date]').val("");
+			}
+		}
+	});
+
+	// 시작시간과 종료시간 비교
+	$('input[name = end_time]').change(function() {
+		const end_time = $('input[name = end_time]').val();
+		const start_time = $('input[name = start_time]').val();
+		const end_date = $('input[name = end_date]').val();
+		const start_date = $('input[name = start_date]').val();
+
+		if (start_time == "") {
+			alert("시작 시간을 먼저 설정해 주세요.");
 			$('input[name = end_time]').val("");
-		}else if(start_time >= end_time){
-			if(end_date == start_date){
-				alert("종료시간은 시작시간 이후로 설정해 주세요.")
+		} else if (start_time >= end_time) {
+			if (end_date == start_date) { // 시작일과 종료일이 같을 경우
+				alert("종료 시간은 시작 시간 이후로 설정해 주세요.")
 				$('input[name = end_time]').val("");
 			}
 		}
-		
 	});
-	
-	//시작 날짜 변경시 유효성 검사
-	   $('input[name = start_date]').change(function(){
-	      var sysdate = new Date(+new Date() + 3240 * 10000).toISOString().split("T")[0];
 
-	      var end_date = $('input[name = end_date]').val();
-	      var start_date = $('input[name = start_date]').val();
-	      
-	      
-	      if(start_date != "" && end_date != ""){
-	         if(start_date > end_date){
-	            alert("시작날짜를 종료날짜와 같거나 이후로 설정해 주세요.")
-	            $('input[name = start_date]').val("");
-	         }
-	      }	      
-	   });
-	
 })
